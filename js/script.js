40d9b91bbc1388572a61a52850085b6232ead8a9
Ailson <robertofeitosa06@gmail.com>
2022-09-27 23:05:24 -0400

    adicionando galeria

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ const element = document.getElementById("current-year");
 if (element) {
   element.innerText = new Date().getFullYear();
 }
+
+const imagesThumbnail = document.querySelectorAll(
+  ".gallery .image-options img"
+);
+const imageActive = document.querySelector(".gallery .image-active img");
+
+const cleanActiveThumbnails = () => {
+  imagesThumbnail.forEach((image) => {
+    image.classList.remove("active");
+  });
+};
+
+imagesThumbnail.forEach((image) => {
+  image.addEventListener("click", ({ target }) => {
+    if (imageActive) {
+      cleanActiveThumbnails();
+      image.classList.add("active");
+
+      const path = target.getAttribute("src");
+      imageActive.setAttribute("src", path.replace("-p", ""));
+    }
+  });
+});
